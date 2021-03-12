0c46df2a859020bd5424a9b194d2b3787d6b514e
Robin Klaus <rmklaus12@gmail.com>
2021-03-12 16:07:27 +1100

    added js code for stick back to top button and added z-index to cta button

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const imgContent = document.querySelectorAll('.img-content-hover');
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-function showImgContent(e) {
-    for (var i = 0; i < imgContent.length; i++) {
-        x = e.pageX;
-        y = e.pageY;
-        imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
-};
+}
 
-document.addEventListener('mousemove', showImgContent);
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
